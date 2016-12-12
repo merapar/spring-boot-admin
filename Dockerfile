6356e007a22a3d7e8e5aaa7bab421d3c31e15442
jan fockaert <jan.fockaert@merapar.com>
2016-12-12 15:34:34 +0100

    Build fat jar and run after mvn package

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,14 @@
 # Docker file for Spring Boot Admin
 #
 
-FROM maven:3.3.9-jdk-8-onbuild-alpine
+FROM maven:3.3.9-jdk-8-alpine
 MAINTAINER Merapar
 
 ENV TZ UTC
+
+RUN     mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+ADD     . /usr/src/app
+
+CMD     mvn package && java -jar target/spring-boot-admin-1.4.4.jar
--- src/main/java/com/merapar/springbootadmin/Application.java
+++ src/main/java/com/merapar/springbootadmin/Application.java
@@ -3,10 +3,10 @@ package com.merapar.springbootadmin;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableAdminServer
 public class Application {
     public static void main(String[] args) {
